2f8d98c7c63a1fce5e9d514a01bdbe9fcc640c9e
Joanmacat <joanmacat@gmail.com>
2022-09-28 13:29:38 +0200

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,12 +2,12 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Systam COEX'
+copyright = '2022, COEX'
+author = 'Marc Juanmartí'
 
-release = '0.1'
-version = '0.1.0'
+release = '0.0.1'
+version = '0.0.1'
 
 # -- General configuration
 
